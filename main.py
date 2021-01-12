d247a3b267a12970720d65886bcc682e6a16afbf
Nguyen Alex <110190@eesd.org>
2021-01-12 00:08:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#This is a comment, comments are not run by the compiler
+#output: Imformation that the computer prints out, what you see printed on the right hand side
+
+print("Hello World")
+
+# Name, grade, favorite book, and favorite superhero
+
+print("My name is Alex Nguyen.")
+print("I am in 7th grade.")
+print("My favorite book is Harry Potter and the Deathly Hallows.")
+print("My favorite superhero is Antman from the movies.")
+
+print("Hello")
+print("World")
+
+# Concatenation: add two strings together using '+'
+
+print("Hello" + " World")
